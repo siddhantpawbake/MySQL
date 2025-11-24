e141fb7dff43c8df9af3da61c0ab50218f434482
Siddhant <sidpawbake15@gmail.com>
2025-11-24 15:46:14 +0530

    Optimized!

--- #1581_CustomerWhoDidntMakeTransaction.sql
+++ #1581_CustomerWhoDidntMakeTransaction.sql
@@ -3,3 +3,11 @@ FROM Visits v
 LEFT JOIN Transactions t ON v.visit_id = t.visit_id
 WHERE t.transaction_id IS NULL
 GROUP BY customer_id;
+
+SELECT customer_id,
+COUNT(*) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (
+    SELECT visit_id FROM Transactions
+)
+GROUP BY customer_id;
